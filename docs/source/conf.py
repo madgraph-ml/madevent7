6990cfa5ee5c71f166311f59b075c38ca983b737
Theo Heimel <info@theovention.de>
2025-12-29 17:48:51 +0100

    add sphinx documentation

---
+++ docs/source/conf.py
@@ -0,0 +1,42 @@
+import os
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "madevent7"
+copyright = "2025, Theo Heimel"
+author = "Theo Heimel"
+release = "0.2.2"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    # "sphinx.ext.napoleon",
+    # "sphinx.ext.linkcode",
+    "sphinx_autodoc_typehints",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
+html_static_path = ["_static"]
+html_theme_options = {
+    "sidebar_hide_name": True,
+    # "light_logo": "logo-light.png",
+    # "dark_logo": "logo-dark.png",
+}
+
+autoclass_content = "both"
+# add_module_names = False
+typehints_fully_qualified = False
---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+madevent7 documentation
+=======================
+
+blabla
+
+
+.. toctree::
+   :maxdepth: 1
+   :caption: Usage
+   :hidden:
+
+   installation
+
+.. toctree::
+   :maxdepth: 1
+   :caption: Reference
+   :hidden:
+
+   Python API <python-api>
+   C++ API <cpp-api>
